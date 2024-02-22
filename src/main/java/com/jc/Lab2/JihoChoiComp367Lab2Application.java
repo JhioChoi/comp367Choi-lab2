644523a687b65eee1bade5b1d4dee8ef962deb04
Jiho Choi <jchoi156@my.centennialcollege.ca>
2024-02-22 15:14:31 -0500

    first commit

---
+++ src/main/java/com/jc/Lab2/JihoChoiComp367Lab2Application.java
@@ -0,0 +1,13 @@
+package com.jc.Lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihoChoiComp367Lab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JihoChoiComp367Lab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/Lab2/JihoChoiComp367Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.Lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JihoChoiComp367Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
